1784594571cff23a4372694a9dc6676a73208435
Sola Jegede <sjegede1@gmail.com>
2023-05-31 08:10:11 -0500

    Start week 2 day 3

---
+++ Week2/Week2Day1/script.js
@@ -0,0 +1,18 @@
+// divided 3 ----> fizz
+// divided by 5 ---> buzz
+// divided by 3 and 5 ---> fizzbuzz
+
+for (let i =1;i<100;i++) {
+    if (i%3==0 && i%5===0) {
+        console.log('fizzbuzz')
+    }
+    else if (i%3 === 0) {
+        console.log('fizz')
+    }
+    else if (i%5===0) {
+        console.log('buzz')
+    }
+    else {
+        console.log(i)
+    }
+}
