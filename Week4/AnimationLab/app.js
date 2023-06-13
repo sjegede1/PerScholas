a849f747594b630cc044a3bffbb3a4165154e0eb
Sola Jegede <sjegede1@gmail.com>
2023-06-13 14:01:04 -0500

    animation lab complete

--- Week4/AnimationLab/app.js
+++ Week4/AnimationLab/app.js
@@ -1,4 +1,8 @@
 const makeBoxMove = (boxNum) => {
     let boxClassName = `.still-box${boxNum}`;
-    document.querySelector(boxClassName).classList.toggle('moving-box')
+    if (boxNum==1) {
+        document.querySelector(boxClassName).classList.toggle('moving-box')
+    } else if (boxNum==2) {
+    document.querySelector(boxClassName).classList.toggle('spinning-box')
+    }
 }
