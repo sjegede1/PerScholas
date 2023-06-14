6786bff78b526fd506d52d540582b9c5649705cd
Sola Jegede <sjegede1@gmail.com>
2023-06-14 15:54:47 -0500

    Just finished making the podium

--- Week4/AnimationHW/app.js
+++ Week4/AnimationHW/app.js
@@ -1,69 +1,113 @@
 // TOGGLE CLASS TO HIGHLIGHT SELCTION
 const selectRacer = (player) => {
-    document.querySelector(`.${player}`).classList.toggle('racer-selected')
-}
-
-
-
+  document.querySelector(`.${player}`).classList.toggle("racer-selected");
+};
 
 // ANIMATION CODE
 
 let options = {
-    // sync options
-    duration: 10*1000,
-    iterations: 1,
-  }
+  // sync options
+  duration: 10 * 1000,
+  iterations: 1,
+};
 
 const raceKeyFrames = (radius) => {
-    let frames = [];
-    let x=0;let y=0
-    // First straight
-    for (y;y<150;y+=20){
-        frames.push({transform:`translateX(${x}px) translateY(${y}px)`})
-    }
+  let frames = [];
+  let x = 0;
+  let y = 0;
+  // First straight
+  for (y; y < 150; y += 20) {
+    frames.push({ transform: `translateX(${x}px) translateY(${y}px)` });
+  }
 
-    // First Turn
-    let delta =  Math.PI/20;
-    // let radius = 225;
-    let tempx; let tempy;
-    for (let i=Math.PI; i>0;i-=delta) {
-        tempx = radius+radius*Math.cos(i);
-        tempy = y+radius*Math.sin(i);
-        frames.push({transform:`translateX(${tempx}px) translateY(${tempy}px)`})
-        
-    }
-    x = tempx;
-    y = tempy;
+  // First Turn
+  let delta = Math.PI / 20;
+  // let radius = 225;
+  let tempx;
+  let tempy;
+  for (let i = Math.PI; i > 0; i -= delta) {
+    tempx = radius + radius * Math.cos(i);
+    tempy = y + radius * Math.sin(i);
+    frames.push({ transform: `translateX(${tempx}px) translateY(${tempy}px)` });
+  }
+  x = tempx;
+  y = tempy;
 
-    // Second Straight
+  // Second Straight
 
-    for (y;y>0;y-=5){
-        frames.push({transform:`translateX(${x}px) translateY(${y}px)`});
-    }
-    // Final Turn
-    for (let i=0; i>-1*Math.PI;i-=delta) {
-        tempx = radius+radius*Math.cos(i);
-        tempy = radius*Math.sin(i);
-        frames.push({transform:`translateX(${tempx}px) translateY(${tempy}px)`})
-        
-    }
+  for (y; y > 0; y -= 5) {
+    frames.push({ transform: `translateX(${x}px) translateY(${y}px)` });
+  }
+  // Final Turn
+  for (let i = 0; i > -1 * Math.PI; i -= delta) {
+    tempx = radius + radius * Math.cos(i);
+    tempy = radius * Math.sin(i);
+    frames.push({ transform: `translateX(${tempx}px) translateY(${tempy}px)` });
+  }
 
-    return frames
-}
-const radii = [264,220,170,120]
-const raceTimes = [7000 + 3000*Math.random(),7000 + 3000*Math.random(),7000 + 3000*Math.random(),7000 + 3000*Math.random()]
-const allRacers = document.querySelectorAll('.racer')
+  return frames;
+};
+const racerNames = ["peterperfect", "penelope", "dastardly", "cavemen"];
+const radii = [264, 220, 170, 120];
+const raceTimeFunc = function () {
+  return 7000 + 3000 * Math.random();
+};
+const raceTimes = [];
+
+const allRacers = document.querySelectorAll(".racer");
+const awardWinnersButton = document.querySelector('.award-winners');
+
 const startRace = () => {
-    
-    allRacers.forEach(
-        (element,index) => {
-            let currentElement = element;
-            options.duration = raceTimes[index];
-            currentElement.animate(raceKeyFrames(radii[index]),options)
-        }
-    )
+    raceTimes.splice(0,raceTimes.length)
+  raceTimes.push(
+    raceTimeFunc(),
+    raceTimeFunc(),
+    raceTimeFunc(),
+    raceTimeFunc()
+  );
+  allRacers.forEach((element, index) => {
+    let currentElement = element;
+    options.duration = raceTimes[index];
+    currentElement.animate(raceKeyFrames(radii[index]), options);
+  });
+//   Make award-winners button clickable
+    awardWinnersButton.disabled = false
+};
+
+// TODO: Make Podium Toggle
+// Create podium grid in HTML using grid and borders
+// THe functions below will be triggered when we award the winners
+const createEntries = (times) => {
+  let entries = {};
+  racerNames.forEach((element, index) => {
+    entries[element] = times[index];
+  });
+  return entries;
+};
+
+
+const sortWinners = (obj) => {
+    return Object.entries(obj).sort((a,b) => b[1]-a[1]).map(el=>el[0]).reverse()
 }
 
-// TODO: MAke Podoium Toggle
-// TODO: Vary racer speeds to create suspense 
-// 
+let firstRacer = document.querySelector('#first-racer')
+let secondRacer = document.querySelector('#second-racer')
+let thirdRacer = document.querySelector('#third-racer')
+
+awardWinnersButton.onclick = () => {
+    let entries = createEntries(raceTimes);
+    let sortedWinnersArray = sortWinners(entries)
+    console.log(document.querySelector('.'+sortedWinnersArray[0]))
+
+    firstRacer.innerHTML = document.querySelector('.'+sortedWinnersArray[0]).innerHTML
+    secondRacer.innerHTML = document.querySelector('.'+sortedWinnersArray[1]).innerHTML
+    thirdRacer.innerHTML = document.querySelector('.'+sortedWinnersArray[2]).innerHTML
+    
+}
+
+
+// TODO: Add bgColor, add font style
+// TODO: Vary racer speeds to create suspense
+
+//FUTURE WORK
+//TODO: Make Robot Dancing Game
